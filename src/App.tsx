6341fe9b4c6acd5c30194c141c119b8b986c2c26
Juanmla <juanmlamperti@gmail.com>
2025-04-04 08:21:25 -0700

    initial setup and basic project config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <h1>Crypto Asset Tracker</h1>
+      <p>Track your favourite crypto stats</p>
+    </div>
+  );
+}
+
+export default App;
